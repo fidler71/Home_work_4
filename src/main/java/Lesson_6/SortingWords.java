0606ebdcd89d6895fa7444ddfb13afadcddf05d1
Ivan Ionov <IAIonov.SBT@sberbank.ru>
2018-03-27 16:28:16 +0300

    Commit

---
+++ src/main/java/Lesson_6/SortingWords.java
@@ -0,0 +1,12 @@
+package Lesson_6;
+
+import java.util.Collections;
+import java.util.List;
+
+public class SortingWords {
+
+	public static List<String> toSort(List<String> list) {
+		Collections.sort(list);
+		return list;
+	}
+}
